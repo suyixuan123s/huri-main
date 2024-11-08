bf77b02111e7c28229b036ec42cd313d05be0c26
suyixuan123s <suyixuan19981215@gmail.com>
2024-11-08 13:53:20 +0800

    Add best.pt to .gitignore to prevent future uploads

--- download_command.py
+++ download_command.py
@@ -1,2 +1,2 @@
-pip download -r requirements.txt -d ../package
-pip install --no-index --find-links=../package -r requirements.txt
+# pip download -r requirements.txt -d ../package
+# pip install --no-index --find-links=../package -r requirements.txt
